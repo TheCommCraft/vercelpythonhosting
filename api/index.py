550a30d42dcc395a0c0134398ac87f061e40dfda
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-04-23 18:01:19 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from threading import Thread
-import time
+import time, requests
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
 # Example code
 
 app = Flask(__name__)
@@ -10,17 +23,11 @@ app = Flask(__name__)
 def home():
     return 'Hello, World! This is TheCommCraft'
 
-@app.route('/about')
+@app.route('/about/')
 def about():
-    return f'About {t} seconds running.'
+    return f'About'
 
-start_time = time.time()
-t = 0
-
-def main():
-    while True:
-        time.sleep(10)
-        t = time.time() - start_time
-
-thread = Thread(target=main)
-thread.start()
+@app.route("/keep_alive/")
+def keep_alive():
+    response = requests.get(request.args.get("url") or 'https://abrupt-imaginary-text.glitch.me/hello', headers=headers)
+    return "probably alive"
