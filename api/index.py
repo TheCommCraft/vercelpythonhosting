89e120ea4b5754ff990b426f9be31b0910ad04c7
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2025-08-10 00:41:50 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -107,8 +107,7 @@ def send_email():
 def malimg():
     file = io.BytesIO("""<svg xmlns="http://www.w3.org/2000/svg" width="400" height="400" viewBox="0 0 124 124" fill="none">
     <rect width="124" height="124" rx="24" fill="#000000"/>
-    <image href="https://willroll.thecommcraft.de/img81513982"></image>´
-       
+    <img src="data:image/svg+xml;utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 124 124' fill='none'><image href='https://willroll.thecommcraft.de/img81513982'></image></svg>" />   
     </svg>""".encode("utf-16"))
     return send_file(
         file,
